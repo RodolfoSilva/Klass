cc0c633516b696c8ce4bd03ea6847a5305c5072c
Rodolfo Silva <contato@rodolfosilva.com>
2014-10-18 23:18:46 -0300

    Correções e melhorias na criação de classes singleton

--- src/plugins/klass.singleton.js
+++ src/plugins/klass.singleton.js
@@ -4,25 +4,25 @@
 //     Distribuído sob a licença MIT.
 
 (function(Klass) {
+    var root = this;
     // Plugin para criação de classes singleton
     Klass.singleton = function () {
-        var args = arguments;
-        if (args.length == 2 && args[0].getInstance) {
-            var Constructor = args[0].getInstance(__extending);
-            // we're extending a singleton swap it out for it's class
-            if (Constructor) { args[0] = Constructor; }
-        }
-        return (function (args){
-            // store instance and class in private variables
-            var instance = false;
-            var Constructor = Klass.extend.apply(args.callee, args);
-            return {
-                getInstance: function () {
-                    if (arguments[0] == __extending) return Constructor;
-                    if (instance) return instance;
-                    return (instance = new Constructor());
-                }
-            };
-        })(args);
+
+        var instance;
+
+        var Constructor = Klass.create(Klass.create.apply(root, arguments), {
+            initialize: function ($super) {
+                if (instance) return instance;
+                instance = Constructor.instance = this;
+                return $super.apply(this, Array.prototype.slice.call(arguments, 1));
+            }
+        });
+
+        Constructor.getInstance = function () {
+            if (instance) return instance;
+            return new Constructor();
+        };
+
+        return Constructor;
     };
 }(Klass));
